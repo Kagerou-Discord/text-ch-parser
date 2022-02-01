267b4a61f2c75d98e78400d0157318dbe1217e68
Lucky3028 <Lucky3028@users.noreply.github.com>
2022-02-01 15:29:30 +0900

    refactor(ext): 変数名や条件分岐をわかりやすく

--- channel_slice_ext.go
+++ channel_slice_ext.go
@@ -4,16 +4,17 @@ import orderedmap "github.com/wk8/go-ordered-map"
 
 // GroupByString groups elements into a map keyed by string. See: http://clipperhouse.github.io/gen/#GroupBy
 func (rcv ChannelSlice) GroupByStringExt(fn func(Channel) string) orderedmap.OrderedMap {
-	mm := orderedmap.New()
-	for _, v := range rcv {
-		key := fn(v)
-		a, b := mm.Get(key)
-		if !b {
-			var ss ChannelSlice
-			mm.Set(key, append(ss, v))
+	om := orderedmap.New()
+	for _, ch := range rcv {
+		key := fn(ch)
+		chSlice, exists := om.Get(key)
+
+		if exists {
+			om.Set(key, append(chSlice.(ChannelSlice), ch))
 		} else {
-			mm.Set(key, append(a.(ChannelSlice), v))
+			var slice ChannelSlice
+			om.Set(key, append(slice, ch))
 		}
 	}
-	return *mm
+	return *om
 }
